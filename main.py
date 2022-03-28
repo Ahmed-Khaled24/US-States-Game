b11d2b64edf114650a7f01685deb63e7c2f281c8
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2022-03-29 00:08:27 +0200

    Add freature to detect if the user answer all the states

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import pandas as pd
 import turtle
 from State_Name import Name
 
-
 # initialize the screen
 screen = turtle.Screen()
 screen.setup(width=800, height=550)
@@ -10,12 +9,12 @@ screen.bgpic("blank_states_img.gif")
 screen.title("State guess game")
 screen.tracer(0)
 
-# prepare the states data
+# prepare the states' data using pandas
 states_data = pd.read_csv("50_states.csv")
 states_data.state = states_data.state.str.lower()
 
-
 game_on = True
+user_win = False
 score = 0
 while game_on:
     screen.update()
@@ -29,19 +28,28 @@ while game_on:
         game_on = False
         break
 
+        # If the user didn't enter e or E for exit.
     if user_ans not in ["e", "E"]:
-        ans_pos = states_data[states_data["state"] == user_ans]  # returns a dataframe contains the row
-                                                                 # of the user answer if it exists.
+        ans_pos = states_data[states_data["state"] == user_ans]  # Returns a dataframe contains the row
+        # of the user answer if it exists.
         if not ans_pos.empty:
+            # Get the coordinates of the state.
             pos = (ans_pos["x"].item(), ans_pos["y"].item())
             state_name.move_to(pos)
             state_name.type(user_ans.title(), 8)
             score += 1
 
+        # If the user answer all the states the game ends
+        if score == 50:
+            game_on = False
+            user_win = True
+
     else:
         game_on = False
 
-
 bye = Name()
-bye.type("Click on the screen to exit", 20)
+if not user_win:
+    bye.type("Click on the screen to exit", 30)
+else:
+    bye.type("Congratulations you win", 30)
 screen.exitonclick()
